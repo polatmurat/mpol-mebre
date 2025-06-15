c29c5d21d0dc4f228f2dbd0ae8d9eb0af592894b
polatmurat <bomch4nte@gmail.com>
2025-06-15 20:52:55 +0300

    Speech Product Management, Remove Products from Cart

--- dashboard-project/deneme.js
+++ dashboard-project/deneme.js
@@ -5,6 +5,22 @@ todos.forEach((yapilacakolan) => {
     console.log(yapilacakolan);
 });
 
+
+const denemeMetni = 'araba kırmızı renkte görünüyor.';
+
+let result = denemeMetni.includes("kırm");
+
+console.log(result);
+
+
+const command = "25 numaralıyı sil";
+
+result = parseInt(command);
+
+console.log(result);
+
+
+
 // for(let i = 0; i < todos.length; i++) {
 //     console.log(todos[i]);
 // }
